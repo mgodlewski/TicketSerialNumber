89ba668d5d0048addfbc6d68fb5e39d380f8133e
Mathieu Godlewski <mathieu.godlewski@orange.com>
2018-04-17 14:00:24 +0200

    chore: Ajout d'un modèle de ticket à personnaliser
    Script plus verbeux

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-modele_ticket=${1?Modèle de ticket}
+modele_carnet=${1?Modèle de Carnet}
 nombre_ticket=${2?Nombre de ticket}
 
 stamp_file=$(mktemp --suffix=ticket_stamp)
 
 numero=1
+tickets_par_modele=50
 
 while [ $numero -lt $nombre_ticket ]
 do
+  echo -n "Generation Carnets contenant tickets $numero à $(($numero + $tickets_par_modele)) ..."
   ./generateStamp.sh $numero > "$stamp_file"
-  pdftk "$modele_ticket" multistamp $stamp_file output carnets_tombolat_$(printf %05d $numero).pdf
-  numero=$(( $numero + 50))
+  pdftk "$modele_carnet" multistamp $stamp_file output carnets_tombolat_$(printf %05d $numero).pdf
+  echo " carnets_tombolat_$(printf %05d $numero).pdf"
+  numero=$(( $numero + $tickets_par_modele))
 done
 
